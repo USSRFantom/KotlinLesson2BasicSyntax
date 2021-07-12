75f74c14ed0bb3c7f8b5c8edda0317e49d39c00e
UssrFantom <dimkakrohin@mail.ru>
2021-07-12 22:49:50 +0300

    Initial commit

---
+++ app/src/main/java/space/ussrfantom/kotlinlesson2basicsyntax/MainActivity.kt
@@ -0,0 +1,22 @@
+package space.ussrfantom.kotlinlesson2basicsyntax
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        variables()
+    }
+
+    fun variables(){
+        val name = "Семен"  //переменная без типа, тип определяется автоматически
+        var name2 = "Астап"  //константаБ переменная с неизменяемым значением как final  в java
+        val name3 = name + " " + "и" + " " + name2
+        Log.i("------->>>>>>проверка!", name3)
+    }
+
+
+}
