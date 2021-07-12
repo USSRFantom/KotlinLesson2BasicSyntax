2609cfb30cea6c58ddf09c82813d6fe5a4fab074
UssrFantom <dimkakrohin@mail.ru>
2021-07-12 23:25:42 +0300

    nullableTypes

--- app/src/main/java/space/ussrfantom/kotlinlesson2basicsyntax/MainActivity.kt
+++ app/src/main/java/space/ussrfantom/kotlinlesson2basicsyntax/MainActivity.kt
@@ -9,6 +9,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         variables()
+        nullableTypes()
     }
     //Переменные
     fun variables(){
@@ -16,6 +17,14 @@ class MainActivity : AppCompatActivity() {
         var name2 = "Астап"  //константаБ переменная с неизменяемым значением как final  в java
         val name3 = name + " " + "и" + " " + name2
         Log.i("------->>>>>>проверка!", name3)
+    }
+
+            //нулабельные типы
+    fun nullableTypes(){
+        val types: String? = null  //создаем переменную которая хранит в себе null. именно знак ? дает это сделать
+        Log.i("------->>>>>>проверка!", types?.length.toString()) //проверка, если вдруг у нас переменная null, чтобы не выпадал эксепшн. проверка на null
+        val types2 = types?.length?:0  //проверяем если переменная не имеет значения присваиваем ей 0, если не то присваиваем необходимое число
+
     }
 
 
